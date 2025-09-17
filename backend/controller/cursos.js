f98fd99f058db2f4b7190536ddb9eeee02ef71c8
Ángel Pastrana <Angel Pastranaangelpastrana0333@gmail.com>
2025-09-17 19:12:10 -0300

    agrego router de cursos, y modificaciones en index y controller cursos

--- backend/controller/cursos.js
+++ backend/controller/cursos.js
@@ -2,11 +2,24 @@ const { connection } = require('../config/dataBase');
 
 // Obtener todos los cursos
 const mostrarCursos = (req, res) => {
-    connection.query('SELECT * FROM Cursos', (error, result) => {
+    connection.query('SELECT * FROM cursos', (error, result) => {
         if (error) {
             return res.status(500).json({ error: 'Error al obtener los cursos' });
         }
         res.json(result);
+    });
+};
+// Obtener un curso por ID
+const mostrarCurso = (req, res) => {
+    const { id } = req.params;
+    connection.query('SELECT * FROM cursos WHERE id_curso  = ?', [id], (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: 'Error al obtener el curso' });
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'Curso no encontrado' });
+        }
+        res.json(results[0]);
     });
 };
 
@@ -17,7 +30,7 @@ const crearCurso = (req, res) => {
         return res.status(400).json({ error: 'Faltan datos requeridos' });
     }
     connection.query(
-        'INSERT INTO Cursos (nombre, descripcion) VALUES (?, ?)',
+        'INSERT INTO cursos (nombre, descripcion) VALUES (?, ?)',
         [nombre, descripcion],
         (error, result) => {
             if (error) {
@@ -33,7 +46,7 @@ const actualizarCurso = (req, res) => {
     const { id } = req.params;
     const { nombre, descripcion } = req.body;
     connection.query(
-        'UPDATE Cursos SET nombre = ?, descripcion = ? WHERE id = ?',
+        'UPDATE cursos SET nombre = ?, descripcion = ? WHERE id = ?',
         [nombre, descripcion, id],
         (error, result) => {
             if (error) {
@@ -48,7 +61,7 @@ const actualizarCurso = (req, res) => {
 const eliminarCurso = (req, res) => {
     const { id } = req.params;
     connection.query(
-        'DELETE FROM Cursos WHERE id = ?',
+        'DELETE FROM cursos WHERE id_curso = ?',
         [id],
         (error, result) => {
             if (error) {
@@ -61,7 +74,8 @@ const eliminarCurso = (req, res) => {
 
 module.exports = {
     mostrarCursos,
+    mostrarCurso,
     crearCurso,
     actualizarCurso,
     eliminarCurso
-};
+}
